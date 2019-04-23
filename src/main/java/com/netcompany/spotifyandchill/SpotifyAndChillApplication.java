4fe1d8e0fcbd804b707140a8da1aaf8560f7c57a
Bang <mariusmbang@gmail.com>
2019-04-23 12:52:32 +0200

    Initial commit

---
+++ src/main/java/com/netcompany/spotifyandchill/SpotifyAndChillApplication.java
@@ -0,0 +1,13 @@
+package com.netcompany.spotifyandchill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyAndChillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyAndChillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/netcompany/spotifyandchill/SpotifyAndChillApplicationTests.java
@@ -0,0 +1,16 @@
+package com.netcompany.spotifyandchill;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpotifyAndChillApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
